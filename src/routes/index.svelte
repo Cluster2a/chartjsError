78a2de43bc4576db996730e143eebd8cfc120591
ahirsch <Esyon4711#>
2022-07-14 22:46:44 +0200

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import Chart from 'chart.js/auto/auto.esm';
+    import { onMount } from 'svelte';
+
+    let chartRef: HTMLCanvasElement;
+
+    // following code is woring with:
+    // @sveltejs/kit: 1.0.0-next.372
+    // vite: 2.9.14
+    onMount(() => {
+        new Chart(chartRef, {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45]
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    });
+</script>
+
+<canvas bind:this={chartRef} />
